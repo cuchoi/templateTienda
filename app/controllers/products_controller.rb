9c257a58c780053e47ab1e57e73e22c029a5d92a
Fernando Irarrazaval <fbirarra@uc.cl>
2013-10-31 18:23:16 -0300

    Arreglado el form y el tema del login

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,8 @@
 
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  
+  before_filter :authenticate_user!, only: [:myproducts]
+
   # GET /products
   # GET /products.json
   def index
