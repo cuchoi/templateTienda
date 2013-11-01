8b61849f2587f1281b2e1883ab5aeec35cda65d5
Fernando Irarrazaval <fbirarra@uc.cl>
2013-10-31 23:04:41 -0300

    Remove the now ignored directory vendor/bundle"

--- vendor/bundle/gems/nokogiri-1.6.0/ext/nokogiri/siteconf20131031-3343-1x3stgx.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20131031-3343-1jrtn8e"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
